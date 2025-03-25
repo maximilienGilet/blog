7df0bf4fa1e758d3a013d6022e3c7084527d18d5
Maximilien Gilet <maximilien248@gmail.com>
2025-03-25 17:03:35 +0100

    Add projets collection and page, update Header for new navigation option

--- src/content/config.ts
+++ src/content/config.ts
@@ -23,4 +23,22 @@ const blog = defineCollection({
     }),
 });
 
-export const collections = { blog };
+const projects = defineCollection({
+  type: "content",
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      description: z.string(),
+      pubDate: z.coerce.date(),
+      heroImage: image().optional(),
+      ogImage: image().refine(img => img.width >= 1200 && img.height >= 630, {
+        message: "OpenGraph image must be at least 1200 X 630 pixels!",
+      }),
+      link: z.string().optional(),
+      github: z.string().optional(),
+      tags: z.array(z.string()).default([]),
+      featured: z.boolean().optional().default(false),
+    }),
+});
+
+export const collections = { blog, projects };
